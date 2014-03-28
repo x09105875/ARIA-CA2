5c2a3d2ccbf371fc98b95f41740b07ab35db677d
Brian Shortall <bshortallsmail@yahoo.co.uk>
2014-03-28 11:23:27 +0000

    newchanges2

--- javascripts/AriaJS.js
+++ javascripts/AriaJS.js
@@ -15,8 +15,14 @@
 	function convertToC() {
 	        var fTempVal = parseFloat(document.getElementById('fTemp').value);
 	        var cTempVal = (fTempVal - 32) * (5 / 9);
+			
+			if(fTempVal <= -459.4){
+			alert("All Values entered should be greater than -459.4 Farenheit to be converted");
+			}
+			else{
 	        document.getElementById('cTemp').value = cTempVal;
 	        return false;
+			}
 	    }
 
 //JS function for converting farenheit to centigrade 
@@ -24,7 +30,13 @@
 	function convertToF() {
 	        var cTempVal = parseFloat(document.getElementById('cTemp').value);
 	        var fTempVal = (cTempVal * (9 / 5)) + 32;
+			
+			if(cTempVal <= -273){
+			alert("All Values entered should be greater than -273 Centigrade to be converted");
+			}
+			else{	
 	        console.log(fTempVal);
 	        document.getElementById('fTemp').value = fTempVal;
 	        return false;
+			}
 	    }
